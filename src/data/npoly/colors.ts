4eb393967b10f6f39d8fe648b1f28eda18049e72
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-04-08 21:59:56 +0600

    remove unnecessary files

--- src/data/npoly/colors.ts
+++
@@ -1,9 +0,0 @@
-import { Color } from '..';
-
-export const NPOLY: Color[] = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
--- src/data/rainbow/colors.ts
+++
@@ -1,9 +0,0 @@
-import { Color } from '..';
-
-export const RAINBOW: Color[] = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
