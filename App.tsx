6e2f0f3534e289045add6c8ffa4f452eb4370c0c
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-04-08 22:27:18 +0600

    rename the Stack to RootStack

--- App.tsx
+++ App.tsx
@@ -3,19 +3,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ColorPalette from './src/screens/ColorPalette';
 import Home from './src/screens/Home';
 
-const Stack = createStackNavigator();
+const RootStack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={Home} />
+        <RootStack.Screen
           name="ColorPalette"
           component={ColorPalette}
           options={({ route }: any) => ({ title: route.params.paletteName })}
         />
-      </Stack.Navigator>
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
