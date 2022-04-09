fe979073fde52192fd7f683b232a5d4e800fbe5f
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-04-09 11:43:51 +0600

    add MainStack and RootStack

--- App.tsx
+++ App.tsx
@@ -4,16 +4,29 @@ import ColorPalette from './src/screens/ColorPalette';
 import Home from './src/screens/Home';
 
 const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        name="ColorPalette"
+        component={ColorPalette}
+        options={({ route }: any) => ({ title: route.params.paletteName })}
+      />
+    </MainStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Screen name="Home" component={Home} />
+      <RootStack.Navigator screenOptions={{ presentation: 'modal' }}>
         <RootStack.Screen
-          name="ColorPalette"
-          component={ColorPalette}
-          options={({ route }: any) => ({ title: route.params.paletteName })}
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
         />
       </RootStack.Navigator>
     </NavigationContainer>
