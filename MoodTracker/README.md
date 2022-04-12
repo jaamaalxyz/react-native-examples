f59d331b105a158aa0646574e45bb5b82af48984
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-04-12 19:19:08 +0600

    add Project: MoodTracking

---
+++ MoodTracker/README.md
@@ -0,0 +1,2 @@
+# MoodTracker
+This is my second React Native practice project thought by Kadi Kraman on FrontendMasters
---
+++ MoodTracker/components/Greeting.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const Greeting: React.FC = () => {
+  return <Text>Hello</Text>;
+};
